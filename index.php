3371fb50e71ca06889311fa4d5a66dcfb8b96da1
Avi Kenny <avi@lastmilehealth.org>
2017-08-12 08:50:08 +0000

    Misc setup changes

--- index.php
+++ index.php
@@ -6,22 +6,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name='robots' content='noindex'>
         <title>LMD Liberia MOH</title>
-        <link rel="stylesheet" href="lib/bootstrap-3.2.0-dist/css/bootstrap.min.css"  type="text/css" />
-        <link rel="stylesheet" href="lib/bootstrap-3.2.0-dist/css/bootstrap-theme.min.css"  type="text/css" />
-        <script src="lib/jquery.min.js"></script>
-        <script src="lib/bootstrap-3.2.0-dist/js/bootstrap.min.js"></script>
         <script>
-//        $(document).ready(function(){
-            
-//            window.location = 'www.google.com';
-            
-//        });
+            window.location = 'http://liberiamohdata.com/LMDLiberiaMOH/build/pages/page_deqa.html';
         </script>
     </head>
     
-    <body>
-        
-        <p>Test</p>
-
-    </body>
+    <body></body>
+    
 </html>
