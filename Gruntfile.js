10eef698b48ed72780e7dc055cef26504fdedc6f
Owen Eddins <oeddins@lastmilehealth.org>
2018-04-20 16:36:20 +0000

    Increment appcache file to 8

--- Gruntfile.js
+++ Gruntfile.js
@@ -138,7 +138,7 @@ module.exports = function(grunt) {
                     'build/js/formValidate.*.js',
                     'build/js/loadContents.*.js'
                 ],
-                dest: 'lmdliberiamoh_v7.appcache'
+                dest: 'lmdliberiamoh_v8.appcache'
             }
         }
     });
